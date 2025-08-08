0cfb36500f3e84ca5d6bad550602d9b43aae7efb
Sam C <chuquest.work@gmail.com>
2025-08-08 03:39:09 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+// App.jsx
+    import React from 'react';
+    import { Routes, Route } from 'react-router-dom';
+    import Nav from './components/Nav';
+    import Home from './components/Home';
+    import Form from './components/Form';
+    import Complete from './components/Complete';
+    import Expire from './components/Expire';
+    
+
+    function App() {
+      return (
+        <Routes>
+          <Route path="/nav" element={<Nav />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/Complete" element={<Complete />} />
+          <Route path="/Expire" element={<Expire />} />
+          {/*
+          
+          <Route path="/Control" element={<Control />} />
+          <Route path="/Admin" element={<Admin />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/Confirm" element={<Confirm />} /> */}
+        </Routes>
+      );
+    }
+
+    export default App;
