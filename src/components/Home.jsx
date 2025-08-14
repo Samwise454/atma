d2adc19f97771c82fd7f27288acb16215c47f436
Sam C <chuquest.work@gmail.com>
2025-08-14 16:31:02 +0100

    Timer updated by a week

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -136,7 +136,7 @@ const Home = () => {
             navigate("/Expire");
         }
         else {
-            let targetDate = new Date("Aug 12, 2025 18:00:00").getTime();
+            let targetDate = new Date("Aug 21, 2025 18:00:00").getTime();
 
             //update the countdown every second 
             let update = setInterval(() => {
